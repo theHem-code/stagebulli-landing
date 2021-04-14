241fe6ab5b51dfc0c30de8ce4a38b0506d608995
Peter Hemsing <peterhemsing@gmail.com>
2021-04-14 13:49:17 +0200

    cookie banner

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "../index.css";
 import ReactPlayer from "react-player";
+import CookieBanner from 'react-cookie-banner';
 
 const Homepage = () => {
   return (
@@ -16,6 +17,17 @@ const Homepage = () => {
         </div>
       </div>
       <Footer />
+      <CookieBanner
+      display={true}
+      styles={{
+        banner: { backgroundColor: 'rgba(60, 60, 60, 0.8)' },
+        message: { fontWeight: 400 }
+      }}  
+      message="Diese Website nutzt sogenannte Cookies. Wenn Sie die Website weiter nutzen, stimmen Sie der Verwendung von Cookies zu."
+      onAccept={() => {}}
+      buttonMessage='Okay'
+      dismissOnScroll={false}
+      cookie="user-has-accepted-cookies" />
     </div>
   );
 };
